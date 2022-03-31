c5ebccc515ab6c715c140f8d03e2329c1e463a75
Michael Legleux <mlegleux@ripple.com>
2022-03-30 23:29:10 -0700

    whatu

--- cd/entrypoint.sh
+++ cd/entrypoint.sh
@@ -4,5 +4,6 @@ echo "Whoa! I've entgered it!"
 ls -lha
 
 git clone https://github.com/legleux/clio.git
-ls -lha
-cmake -B clio/build && cmake --build clio/build --parallel $(nproc)
+ls -lha clio
+cd clio 
+cmake -B build && cmake --build build --parallel $(nproc)

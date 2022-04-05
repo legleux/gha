ab7701fcd25c31aeb12bbe1439e0119121079cd5
Michael Legleux <mlegleux@ripple.com>
2022-04-05 11:02:42 -0700

    build

--- up.sh
+++ up.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-
-ls -lh
-echo "am i not really here at all?"
-cmake -B clio_src/build && cmake --build clio_src/build --parallel $(nproc)
+cmake -S clio_src -B clio_src/build 
+cmake --build clio_src/build --parallel $(nproc)

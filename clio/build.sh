20db2e06bf2325d906781428e09716fc7dee54a6
Michael Legleux <mlegleux@ripple.com>
2022-04-11 15:42:08 -0700

    needed that

--- clio/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-ls -lha
-
-# git clone https://github.com/legleux/clio.git
-# cd clio && git checkout develop
-cmake -B build && cmake --build build --parallel $(nproc)
--- clio/lint/script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "am act!"
-cat clang-format.patch
-echo clang-format.patch
